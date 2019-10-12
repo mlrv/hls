12c3029b4fad77893cddab69c8cb451082c9480a
ɯλrv¬ <marco.levrero@gmail.com>
2019-10-12 11:25:37 +0100

    Add utility functions for fancier print (#3)

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,24 @@
 module Main where
 
 import           Data.Foldable    as F
+import           Data.Traversable as T
 import           System.Directory as D
+
+import           Models
 
 main :: IO ()
 main = do
   files <- D.listDirectory "."
-  F.for_ files putStrLn
+  files' <- elaborateFiles files
+  F.for_ files' putFileInfoLn
+
+elaborateFiles :: [FilePath] -> IO [FileInfo]
+elaborateFiles files =
+  T.for files $ \file ->
+    FileInfo file <$> D.getFileSize file <*> D.doesDirectoryExist file
+
+putFileInfoLn :: FileInfo -> IO ()
+putFileInfoLn = putStrLn . renderFileInfo
+
+renderFileInfo :: FileInfo -> String
+renderFileInfo file = _filename file ++ ", " ++ show (_size file) ++ "B"
